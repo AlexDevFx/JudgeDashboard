e5cf2a145db492b688258729603f03b404bf9b7b
Alexey Lopatin <diver.stv@gmail.com>
2019-07-09 16:28:23 +0300

    Fix bug in extra timers

--- JudgeScores/RandomTimerSettings.cs
+++ JudgeScores/RandomTimerSettings.cs
@@ -5,22 +5,48 @@ namespace JudgeScores
 {
 	public class RandomTimerSettings
 	{
+		public RandomTimerSettings()
+		{
+			UpperLimit = 59;
+			LowerLimit = 1;
+		}
+
 		public bool IsEnabled{ get; set; }
 
-		public int LowerLimit { get; set; }
-		public int UpperLimit { get; set; }
+		public int LowerLimit
+		{
+			get { return _lowerLimit; }
+			set
+			{
+				if (value > 0 && value < 60)
+					_lowerLimit = value;
+			}
+		}
+
+		public int UpperLimit
+		{
+			get { return _upperLimit; }
+			set
+			{
+				if (value > 0 && value < 60)
+					_upperLimit = value;
+			}
+		}
 		public string FilePath { get; set; }
 
 		public NumericUpDown LowerLimitControl { get; set; }
 		public NumericUpDown UpperLimitControl { get; set; }
 		public CheckBox IsEnabledControl { get; set; }
 
+		private int _lowerLimit = 1;
+		private int _upperLimit = 59;
+
 		public void UpdateControls()
 		{
-			if (UpperLimitControl != null)
-				UpperLimitControl.Value = Math.Max(59, Math.Min(1, UpperLimit));
-			if (LowerLimitControl != null)
-				LowerLimitControl.Value =  Math.Max(59, Math.Min(1,LowerLimit));
+			if (UpperLimitControl != null && UpperLimit > 0 && UpperLimit < 60)
+				UpperLimitControl.Value = UpperLimit;
+			if (LowerLimitControl != null && LowerLimit > 0 && LowerLimit < 60)
+				LowerLimitControl.Value = LowerLimit;
 			if (IsEnabledControl != null)
 				IsEnabledControl.Checked = IsEnabled;
 		}
